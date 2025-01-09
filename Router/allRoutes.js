a551bc1feb3dd846f7fd05590c5b1b95891ac9b6
MarioB1 <mcbruckert@gmail.com>
2025-01-09 10:05:23 +0000

    avant page contact

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,12 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
-    new Route("/", "Covoiturage", "/pages/covoiturage.html"),];
+    new Route("/", "Accueil", "/pages/home.html",[]),
+    new Route("/covoiturage", "Covoiturage", "/pages/covoiturage.html",[]),
+
+    new Route("/contact", "Contact", "/pages/contact.html",[]),
+];
+
+
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "EcoRide";
