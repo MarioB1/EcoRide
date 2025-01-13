e8816a7940a35a70c4c11321a8a5357232983757
MarioB1 <mcbruckert@gmail.com>
2025-01-13 10:09:30 +0000

    a

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,8 +5,10 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html",[]),
     new Route("/covoiturage", "Covoiturage", "/pages/covoiturage.html",[]),
     new Route("/contact", "Contact", "/pages/contact.html",[]),
-    new Route("/signin", "Connexion", "/pages/signin.html",[]),
-    new Route("/signup", "Inscription", "/pages/signup.html",[]),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html",[]),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html",[]),
+    new Route("/account", "Mon compte", "/pages/auth/account.html",[]),
+
 
 ];
 
