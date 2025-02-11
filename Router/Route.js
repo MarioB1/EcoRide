21fb34b40088a1f88376549728b7f01e6262ab83
MarioB1 <mcbruckert@gmail.com>
2025-02-11 11:42:42 +0000

    maj avant API

--- Router/Route.js
+++ Router/Route.js
@@ -1,8 +1,18 @@
 export default class Route {
-    constructor(url, title, pathHtml, pathJS = "") {
-      this.url = url;
-      this.title = title;
-      this.pathHtml = pathHtml;
-      this.pathJS = pathJS;
-    }
+  constructor(url, title, pathHtml, authorize, pathJS = "") {
+    this.url = url;
+    this.title = title;
+    this.pathHtml = pathHtml;
+    this.pathJS = pathJS;
+    this.authorize = authorize;
+  }
 }
+
+/*
+[] -> Tout le monde peut y accéder
+["disconnected"] -> Réserver aux utilisateurs déconnecté 
+["client"] -> Réserver aux utilisateurs avec le rôle client 
+["admin"] -> Réserver aux utilisateurs avec le rôle admin 
+["employee"] -> Réserver aux utilisateurs avec le rôle employé 
+["admin", "client","employee"] -> Réserver aux utilisateurs avec le rôle client OU admin OU employé
+*/
--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,12 +2,15 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html",[]),
+    new Route("/home", "Accueil", "/pages/home.html",[]),
     new Route("/covoiturage", "Covoiturage", "/pages/covoiturage.html",[]),
     new Route("/contact", "Contact", "/pages/contact.html",[]),
-    new Route("/signin", "Connexion", "/pages/auth/signin.html",[]),
-    new Route("/signup", "Inscription", "/pages/auth/signup.html",[]),
-    new Route("/account", "Mon compte", "/pages/auth/account.html",[]),
+    new Route("/avis", "Avis", "/pages/avis.html",["employe"]),
+    new Route("/trajets", "Trajets", "/pages/trajets.html",["employe"]),
+    new Route("/admin", "Espace administarateur", "/pages/admin.html",["admin"]),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html", ["disconnected"],"/js/auth/signin.js"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html", ["disconnected"], "/js/auth/signup.js"),
+    new Route("/account", "Mon compte", "/pages/auth/account.html", ["connected"]),
 
 
 ];
